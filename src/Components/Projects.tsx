fac172dd6addee9a334e6899b5d633721b775755
codexindia <sudiptobain8@gmail.com>
2023-12-30 18:02:41 +0530

    s

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import { LinkIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Link } from 'react-router-dom'
+
 export default function Projects() {
   return (
     <>
