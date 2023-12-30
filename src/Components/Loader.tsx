2f7570cbf4ab1d4045da2b4273ce9f795cc64c32
codexindia <sudiptobain8@gmail.com>
2023-12-31 03:29:24 +0530

    m

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -3,12 +3,12 @@ import LoadingBar from 'react-top-loading-bar'
 
 
 const Loader = (props: any) => {
-  
+
 
   return (
     <div>
       <LoadingBar
-
+        height={3}
         color='#67d835'
         progress={props.progress}
         onLoaderFinished={() => props.SetProgress(0)}
