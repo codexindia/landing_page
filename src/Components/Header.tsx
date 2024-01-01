dd09876ff891dbf2b03659b44fa913a8a2bf0eba
codexindia <sudiptobain8@gmail.com>
2024-01-01 18:09:29 +0530

    s

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -87,13 +87,13 @@ export default function Header() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <Link
+                    <NavLink
                       key={item.name}
                       to={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </Link>
+                    </NavLink>
                   ))}
                 </div>
                 <div className="py-6">
