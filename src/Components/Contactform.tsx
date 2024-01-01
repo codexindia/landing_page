2f7727ed4adbcee1121a6e8f7de50f6b7d01c514
codexindia <sudiptobain8@gmail.com>
2024-01-01 21:12:26 +0530

    gg

--- src/Components/Contactform.tsx
+++ src/Components/Contactform.tsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 
 export default function ContactForm() {
+  const initialvalues = {
+    fname: "",
+    lname: "",
+    email: "",
+    mobileNumber: "",
+    message: "",
+    recaptha: "",
+  };
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [message, setMessage] = useState("");
+  const [recaptha, setRecaptha] = useState("");
+  function onChange(value: any) {
+    setRecaptha(value);
+  }
+  let handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      console.log(recaptha);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
 
@@ -22,40 +50,44 @@ export default function ContactForm() {
                 Fill in the form
               </h2>
 
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="grid gap-4">
 
                   <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     <div>
                       <label htmlFor="hs-firstname-contacts-1" className="sr-only">First Name</label>
-                      <input type="text" name="hs-firstname-contacts-1" id="hs-firstname-contacts-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="First Name" />
+                      <input type="text" onChange={(e) => setName(e.target.value)} name="hs-firstname-contacts-1" id="hs-firstname-contacts-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="First Name" />
                     </div>
 
                     <div>
                       <label htmlFor="hs-lastname-contacts-1" className="sr-only">Last Name</label>
-                      <input type="text" name="hs-lastname-contacts-1" id="hs-lastname-contacts-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Last Name" />
+                      <input type="text" onChange={(e) => setLastname(e.target.value)} name="hs-lastname-contacts-1" id="hs-lastname-contacts-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Last Name" />
                     </div>
                   </div>
 
 
                   <div>
                     <label htmlFor="hs-email-contacts-1" className="sr-only">Email</label>
-                    <input type="email" name="hs-email-contacts-1" id="hs-email-contacts-1" autoComplete="email" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Email" />
+                    <input type="email" onChange={(e) => setEmail(e.target.value)} name="hs-email-contacts-1" id="hs-email-contacts-1" autoComplete="email" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Email" />
                   </div>
 
                   <div>
                     <label htmlFor="hs-phone-number-1" className="sr-only">Phone Number</label>
-                    <input type="text" name="hs-phone-number-1" id="hs-phone-number-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Phone Number" />
+                    <input type="text" onChange={(e) => setMobileNumber(e.target.value)} name="hs-phone-number-1" id="hs-phone-number-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Phone Number" />
                   </div>
 
                   <div>
                     <label htmlFor="hs-about-contacts-1" className="sr-only">Details</label>
-                    <textarea id="hs-about-contacts-1" name="hs-about-contacts-1" rows={4} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Details"></textarea>
+                    <textarea id="hs-about-contacts-1" onChange={(e) => setMessage(e.target.value)} name="hs-about-contacts-1" rows={4} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-green-400 focus:ring-green-400 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Details"></textarea>
                   </div>
                 </div>
 
+                <ReCAPTCHA onChange={onChange} className='mt-5' style={{ transform: "scale(0.85)", transformOrigin: "0 0" }}
+                  sitekey="6LebaC8bAAAAAK1Ma0X3SHUSv5feLpEytTFTC7Yk"
 
-                <div className="mt-4 grid">
+                />,
+
+                <div className="grid">
                   <button type="submit" className="hover:bg-green-400 w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-indigo text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Send inquiry</button>
                 </div>
 
