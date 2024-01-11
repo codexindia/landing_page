a8bad6f0c1e872debd23fad45450b7f8d3cad551
codexindia <sudiptobain8@gmail.com>
2024-01-11 21:15:15 +0530

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Home from "./Pages/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Service from "./Pages/Service"
 
+
+
 function App() {
   return (<>
     <BrowserRouter>
       <Routes>
-       
+     
         <Route path="/service" element={<Service />} ></Route>
         <Route path="/" element={<Home />} ></Route>
         <Route path="/contact" element={<Contact />} ></Route>
